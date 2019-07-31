fdce2cb79e9f4c9935636bba44c48b1050440515
James Britton <brittonjp@gmail.com>
2019-07-31 16:55:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,6 +44,19 @@ class Student
     end
   end
 
+  def self.first_X_students_in_grade_10(X)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE students.grade 10
+      LIMIT X
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
